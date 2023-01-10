9e1389eca64cd4932867b85c9fffa853a36d0daf
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2023-01-10 16:58:07 +0900

    [Bronze V] Title: 웰컴, Time: 120 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
