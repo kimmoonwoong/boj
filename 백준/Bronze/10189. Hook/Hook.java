322b138581295803f6638717bb12df8f57a60e1b
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2023-01-17 16:37:48 +0900

    [Bronze V] Title: Hook, Time: 108 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
