a98525c1ab4edcaa43732807598aaa5f97818bf1
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2023-01-25 17:21:54 +0900

    [Silver IV] Title: 좋은 단어, Time: 284 ms, Memory: 18672 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int i = 0;i<n;i++){
+            String s = br.readLine();
+
+            if (s.length() % 2 != 0) continue;
+
+            Stack<Character> stack = new Stack<>();
+
+            for (int j = 0;j<s.length();j++){
+                if (stack.size() > 0 && stack.peek() == s.charAt(j)) stack.pop();
+                else stack.push(s.charAt(j));
+            }
+            if (stack.isEmpty()) cnt++;
+
+        }
+        System.out.println(cnt);
+    }
+}
