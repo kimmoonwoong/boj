bc2f6a8bd51a1688e53c572c09b358b9cfb01560
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2023-03-19 18:45:57 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int aa,ab,ac; cin >> aa >> ab >> ac;
+    int ca,cb,cc; cin >> ca >> cb >> cc;
+    
+    cout << ca - ac << " " << cb / ab << " " << cc - aa;
+}
