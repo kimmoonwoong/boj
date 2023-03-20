7ca1c5d96b6bfeab5900dff97ce6d0730c858c49
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2023-03-20 19:51:35 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2268 KB -BaekjoonHub

---
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int arr[1001];
+int dp[1001][31][2];
+int main(void){
+    ios_base :: sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int t,w; cin >> t >> w;
+    for (int i = 1; i <= t; ++i) cin >> arr[i];
+
+    for (int i = 0; i <= w; ++i) {
+        for (int j = 1; j <= t; ++j) {
+            if (i == 0){
+                dp[j][i][0] = dp[j - 1][i][0] + (arr[j] == 1);
+            }
+            else{
+                dp[j][i][0] = max(dp[j - 1][i - 1][1] + (arr[j] == 1), dp[j - 1][i][0] + (arr[j] == 1));
+                dp[j][i][1] = max(dp[j - 1][i][1] + (arr[j] == 2), dp[j - 1][i - 1][0] + (arr[j] == 2));
+            }
+        }
+    }
+    int ans = 0;
+    for (int i = 0; i <= w; ++i) {
+        ans = max(ans, max(dp[t][i][0], dp[t][i][1]));
+    }
+    cout << ans;
+    return 0;
+}
