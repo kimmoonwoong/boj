2f4d595804d4216bce45de4c06a2fe0674ea95f6
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2023-05-04 16:43:23 +0900

    [Silver I] Title: 점프, Time: 132 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException, NumberFormatException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long dp[][] = new long[n][n];
+        int map[][] = new int[n][n];
+
+        for (int i = 0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0;j<n;j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        dp[0][0] = 1;
+        for (int i = 0;i<n;i++){
+            for (int j = 0;j<n;j++){
+                if (i == n - 1 && j == n - 1) break;
+                if (j + map[i][j] < n) dp[i][j + map[i][j]] += dp[i][j];
+                if (i + map[i][j] < n) dp[i + map[i][j]][j] += dp[i][j];
+            }
+        }
+        System.out.println(dp[n - 1][n - 1]);
+    }
+}
