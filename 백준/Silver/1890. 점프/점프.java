6de7fa02dd3bea2096911149564e257f30ef29bf
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2023-05-04 16:49:03 +0900

    [Silver I] Title: 점프, Time: 80 ms, Memory: 11792 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -7,17 +7,12 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         long dp[][] = new long[n][n];
         int map[][] = new int[n][n];
-
+        dp[0][0] = 1;
         for (int i = 0;i<n;i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0;j<n;j++){
                 map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        dp[0][0] = 1;
-        for (int i = 0;i<n;i++){
-            for (int j = 0;j<n;j++){
-                if (i == n - 1 && j == n - 1) break;
+                if (map[i][j] == 0) continue;
                 if (j + map[i][j] < n) dp[i][j + map[i][j]] += dp[i][j];
                 if (i + map[i][j] < n) dp[i + map[i][j]][j] += dp[i][j];
             }
