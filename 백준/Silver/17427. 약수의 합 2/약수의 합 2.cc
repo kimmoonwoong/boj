11eba626fc4971577684bb5d671542f2431b1812
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2023-06-17 18:41:32 +0900

    [Silver II] Title: 약수의 합 2, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/17427. 약수의 합 2/약수의 합 2.cc
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.cc
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
+    ios_base :: sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
     long long n; cin >> n;
     long long ans = 0;
     for(int i = 1;i<=n;i++)ans += (long long)(n / i) * i;
