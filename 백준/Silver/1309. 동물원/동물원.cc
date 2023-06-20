4a5d352c3d963a8caa0201db70e74dbd1af65203
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2023-06-20 16:31:33 +0900

    [Silver I] Title: 동물원, Time: 0 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+long long dp[100001];
+int main(){
+    int n; cin >> n;
+    dp[1] = 3;
+    dp[2] = 7;
+    for(int i = 3;i<=n;i++) dp[i] = (2 * dp[i - 1] + dp[i - 2]) % 9901;
+    cout << dp[n];
+}
