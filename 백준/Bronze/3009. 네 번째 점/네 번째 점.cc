3d91a2f6f5ed3026465ae90ef7535ba97ee1670f
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2023-12-21 21:19:41 +0900

    [Bronze III] Title: 네 번째 점, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.cc
@@ -3,7 +3,6 @@ using namespace std;
 int x[3];
 int y[3];
 int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL);
 	for (int i = 0; i < 3; i++) cin >> x[i] >> y[i];
 	int xs = x[0] ^ x[1] ^ x[2];
 	int ys = y[0] ^ y[1] ^ y[2];
