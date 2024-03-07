d4be23716df16c37e3a58fdde0d7fa9c4677f35b
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2024-03-07 21:08:23 +0900

    [Bronze II] Title: 하늘과 핑크, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31473. 하늘과 핑크/하늘과 핑크.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n; cin >> n;
+    int sum1 = 0, sum2 = 0;
+    for(int i = 0;i<n;i++){
+        int k; cin >> k;
+        sum1 += k;
+    }
+    
+    for(int i = 0;i<n;i++){
+        int k; cin >> k;
+        sum2 += k;
+    }
+    
+    cout << sum2 << " " << sum1 << "\n";
+}
