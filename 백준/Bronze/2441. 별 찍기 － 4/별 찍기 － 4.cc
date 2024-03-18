43294f53948d069777bd8132deb921dbb77c9499
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2024-03-18 23:23:07 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n; cin >> n;
+    
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<i;j++) cout << " ";
+        for(int j = i;j<n;j++) cout <<"*";
+        cout << "\n";
+    }
+}
