9c942f91b51ac4ce3d9d7f9b55a77f3c10f8c99d
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2024-06-04 00:49:03 +0900

    [Bronze II] Title: 마라탕후루, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/31923. 마라탕후루/마라탕후루.cc
+++ 백준/Bronze/31923. 마라탕후루/마라탕후루.cc
@@ -13,34 +13,25 @@ int main(void)
     for (int i = 0; i < n; i++)
         cin >> arrB[i];
 
-    vector<int> ans(n);
+    vector<int> ans(n, -1);
     for (int i = 0; i < n; i++) {
         if (p == q) {
             if (arrA[i] == arrB[i]) {
                 ans[i] = 0;
             }
-            else {
-                cout << "NO";
-                return 0;
-            }
         }
         else if (p > q) {
-            if (arrA[i] > arrB[i] || (arrB[i] - arrA[i]) % (p - q)) {
-                cout << "NO";
-                return 0;
-            }
-            else {
+            if (arrA[i] <= arrB[i] && (arrB[i] - arrA[i]) % (p - q) == 0)
                 ans[i] = (arrB[i] - arrA[i]) / (p - q);
-            }
         }
         else {
-            if (arrB[i] > arrA[i] || (arrA[i] - arrB[i]) % (q - p)) {
-                cout << "NO\n";
-                return 0;
-            }
-            else {
+            if (arrB[i] <= arrA[i] && (arrA[i] - arrB[i]) % (q - p) == 0)
                 ans[i] = (arrA[i] - arrB[i]) / (q - p);
-            }
+        }
+
+        if (ans[i] == -1) {
+            cout << "NO";
+            return 0;
         }
     }
 
