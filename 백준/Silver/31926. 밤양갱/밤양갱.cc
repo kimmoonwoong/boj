426839f341fea525153d8121a335fb915f4e5ce9
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2024-06-04 10:48:39 +0900

    [Silver I] Title: 밤양갱, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/31926. 밤양갱/밤양갱.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <cmath>
+#define ll long long
+#define pii pair<int, int>
+using namespace std;
+
+void solve() {
+	int n; cin >> n;
+	cout << 10 + (int)floor(log2(n));
+}
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	solve();
+}
