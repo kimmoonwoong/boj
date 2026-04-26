06ec9f4bb50f4b3643f88cc0d540882fa133d334
kimmoonwoong <75220547+kimmoonwoong@users.noreply.github.com>
2026-04-26 19:31:00 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	while (true) {
+		int a, b; cin >> a >> b;
+
+		if (a == 0 && b == 0)
+			break;
+
+		if (b % a == 0)
+		{
+			cout << "factor\n";
+			continue;
+		}
+		if (a % b == 0)
+		{
+			cout << "multiple\n";
+			continue;
+		}
+
+		cout << "neither\n";
+	}
+
+}
